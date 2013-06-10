97ce14387f7da8dae4d5c082cdae1d05074b864a
Ryan Cavicchioni <ryan@confabulator.net>
2013-06-09 22:42:17 -0500

    Change the email address to null if an empty string.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,5 +15,10 @@ module Models
       password_hash = BCrypt::Password.new(self.password_hash)
       password_hash == password
     end
+
+    def email=(email)
+      return nil if email.strip.empty?
+      super(email)
+    end
   end
 end
