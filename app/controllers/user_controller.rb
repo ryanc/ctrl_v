ada0bf91f11fdc2d2cb39989c4cd342219a7fcaf
Ryan Cavicchioni <ryan@confabulator.net>
2013-06-12 08:42:53 -0500

    Update the time that the user was last seen.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -25,6 +25,8 @@ class App < Sinatra::Base
     if user
       if user.authenticate(params[:password])
         session[:uid] = user.id
+        user.last_seen_at = Time.now
+        user.save
         redirect to '/pastes'
       end
     end
