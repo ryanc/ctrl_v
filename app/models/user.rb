da3c08e5f548955ea0bbefe4123229c3604e9fd0
Ryan Cavicchioni <ryan@confabulator.net>
2013-06-12 20:14:07 -0500

    Add validation error for blank password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,8 @@ module Models
       errors.add(:username, "The username '#{username}' is already taken.") unless model.where(:username => username).empty?
       errors.add(:username, 'The username cannot be blank.') if !username || username.strip.empty?
       errors.add(:email, 'The email address cannot be blank.') if !email || email.strip.empty?
-      errors.add(:password, 'The passwords do not match.') if password_confirmation || password != password_confirmation
+      errors.add(:password, 'The password cannot be blank.') if !password || password.strip.empty?
+      errors.add(:password_confirmation, 'The passwords do not match.') if password_confirmation || password != password_confirmation
     end
   end
 end
