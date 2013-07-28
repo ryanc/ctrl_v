ad67d16f0daed5cce7615068e6f682e128188405
Ryan Cavicchioni <ryan@cavi.cc>
2013-07-28 14:08:03 -0500

    Added is_spam column.

---
+++ db/migrations/003_add_is_spam_column.rb
@@ -0,0 +1,14 @@
+require 'sequel'
+
+Sequel.migration do
+  up do
+    alter_table :paste do
+      add_column :is_spam, FalseClass, :default => false
+    end
+  end
+  down do
+    alter_table :paste do
+      drop_column :is_spam
+    end
+  end
+end
