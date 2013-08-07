09714c2b058fc85404fc09b7a78173046b1afb64
Ryan Cavicchioni <ryan@cavi.cc>
2013-08-06 23:47:18 -0500

    Removed paste content setter that was not used.

--- app/models/paste_content.rb
+++ app/models/paste_content.rb
@@ -4,13 +4,6 @@ module Models
   class Content < Sequel::Model(:paste_content)
     plugin :timestamps
 
-    def content=(content)
-      content.strip!
-      content.gsub! /\r\n?/, "\n"
-      self.digest = Digest::MD5.hexdigest(content)
-      super(content)
-    end
-
     def to_s
       self.content
     end
