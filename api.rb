04010e6ffba5cf7a39f21d5087383c3262d22e13
Ryan Cavicchioni <ryan@cavi.cc>
2013-09-10 22:24:29 -0500

    Added REST API.

---
+++ api.rb
@@ -0,0 +1,37 @@
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+
+require 'sinatra/base'
+
+class Api < Sinatra::Base
+  use Rack::Auth::Basic, "API requires authentication" do |username, password|
+    user = Models::User.find(:username => username)
+    user && user.authenticate(password)
+  end
+
+  get '/paste/:id' do
+    content_type 'text/plain'
+    paste = Models::Paste.find(:id_b62 => params[:id])
+    halt 404, "Not Found" if paste.nil?
+    paste.content.to_s
+  end
+
+  post '/paste' do
+    username = request.env['REMOTE_USER']
+    paste = Models::Paste.create(
+      :filename => params[:filename],
+      :highlighted => params[:highlighted] || true,
+      :content => params[:content],
+      :user_id => Models::User.find(:username => username).id,
+      :ip_addr => request.ip,
+    )
+    redirect to "/p/#{paste.id_b62}"
+  end
+
+  delete '/paste/:id' do
+    paste = Models::Paste.find(:id_b62 => params[:id])
+    halt 404, "Not Found" if paste.nil?
+    paste.active = false
+    paste.save
+    204
+  end
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,10 @@
 require ::File.expand_path './../app', __FILE__
-run App
+require ::File.expand_path './../api', __FILE__
+
+map '/' do
+  run App
+end
+
+map '/api' do
+  run Api
+end
