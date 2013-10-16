e4bc7b6cdeed38acb871556313cc817217e56b05
Ryan Cavicchioni <ryan@cavi.cc>
2013-10-15 22:23:02 -0500

    Move unicorn pid and socket file.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,8 @@ deploy_to = "/srv/apps/#{application}"
 shared_path = "#{deploy_to}/shared"
 
 worker_processes 2
-listen "#{shared_path}/sockets/unicorn.sock"
-pid "#{shared_path}/pids/unicorn.pid"
+listen "#{shared_path}/tmp/sockets/unicorn.sock"
+pid "#{shared_path}/tmp/pids/unicorn.pid"
 stderr_path "#{shared_path}/log/unicorn.stderr.log"
 stdout_path "#{shared_path}/log/unicorn.stdout.log"
 preload_app true
