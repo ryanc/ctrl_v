1dbeb6306765b916975df0e1a4c2d1998a9aee93
Ryan Cavicchioni <ryan@cavi.cc>
2013-10-26 23:02:57 -0500

    Listen on port 8080 as well as a UNIX domain socket.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,7 @@ shared_path = "#{deploy_to}/shared"
 
 worker_processes 2
 listen "#{shared_path}/tmp/sockets/unicorn.sock"
+listen 8080
 pid "#{shared_path}/tmp/pids/unicorn.pid"
 stderr_path "#{shared_path}/log/unicorn.stderr.log"
 stdout_path "#{shared_path}/log/unicorn.stdout.log"
