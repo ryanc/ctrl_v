e85259ed180497e156e7ff90e5fb8bf8c1a970f2
Ryan Cavicchioni <ryan@cavi.cc>
2014-05-17 18:52:42 -0500

    Revert "Added HTML escaping helper."
    This reverts commit adc8921096f3cf232dad0fa88d7952d4fd3af85d.

--- app.rb
+++ app.rb
@@ -36,9 +36,6 @@ class App < Sinatra::Base
     if session.exists? and flash
       @flash_error = flash[:error] if flash.has?(:error)
       @flash_success = flash[:success] if flash.has?(:success)
-  helpers do
-    def e(text)
-      Rack::Utils.escape_html(text)
     end
   end
 end
