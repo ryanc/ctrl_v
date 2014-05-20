e29134fd00a6062b1ea4ea26b266e290b125b1aa
Ryan Cavicchioni <ryan@cavi.cc>
2014-05-19 21:37:10 -0500

    Removed unused view helper.

--- app/helpers/view.rb
+++
@@ -1,8 +0,0 @@
-class App
-  module ViewHelpers
-    def gravatar(size = 32)
-      gravitar_id = Digest::MD5.hexdigest(self[:email].to_s.strip.downcase)
-      "//www.gravatar.com/avatar/#{gravitar_id}?s=#{size}"
-    end
-  end
-end
