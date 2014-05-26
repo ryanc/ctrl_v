25d4ab6004ebf5156addc04dd2f64c373931f62f
Ryan Cavicchioni <ryan@cavi.cc>
2014-05-25 22:25:05 -0500

    Broke up the paste action into multiple actions.

--- app/controllers/paste_controller.rb
+++ app/controllers/paste_controller.rb
@@ -27,27 +27,43 @@ class App < Sinatra::Base
 
   get '/p/:id' do
     cache_control s_max_age: 86_400
-    @paste = Models::Paste.find(id_b62: params[:id], active: true, spam: false)
+    @paste = paste(params[:id])
     halt(404) if @paste.nil?
-    if params.key? 'raw'
-      content_type 'text/plain'
-      @paste.content.to_s
-    elsif params.key? 'download'
-      headers['Content-Type'] = 'application/octet-stream'
-      headers['Content-Disposition'] = "attachment; filename=#{@paste.filename}"
-      headers['Content-Transfer-Encoding'] = 'binary'
-      @paste.content.to_s
-    elsif params.key? 'delete'
-      halt(403) unless @paste.owner?(current_user)
-      @paste.active = false
-      @paste.save
-      flash[:success] = "Paste ##{@paste.id_b62} has been deleted."
-      redirect to '/new'
-    elsif params.key? 'clone'
-      erb :new
-    else
-      erb :paste
-    end
+    erb :paste
+  end
+
+  get '/p/:id/raw' do
+    cache_control s_max_age: 86_400
+    @paste = paste(params[:id])
+    halt(404) if @paste.nil?
+    content_type 'text/plain'
+    @paste.content.to_s
+  end
+
+  get '/p/:id/download' do
+    cache_control s_max_age: 86_400
+    @paste = paste(params[:id])
+    halt(404) if @paste.nil?
+    headers['Content-Type'] = 'application/octet-stream'
+    headers['Content-Disposition'] = "attachment; filename=#{@paste.filename}"
+    headers['Content-Transfer-Encoding'] = 'binary'
+    @paste.content.to_s
+  end
+
+  get '/p/:id/clone' do
+    @paste = paste(params[:id])
+    halt(404) if @paste.nil?
+    erb :new
+  end
+
+  get '/p/:id/delete' do
+    @paste = paste(params[:id])
+    halt(404) if @paste.nil?
+    halt(403) unless @paste.owner?(current_user)
+    @paste.active = false
+    @paste.save
+    flash[:success] = "Paste ##{@paste.id_b62} has been deleted."
+    redirect to '/new'
   end
 
   get '/latest' do
@@ -60,6 +76,10 @@ class App < Sinatra::Base
   end
 
   private
+
+  def paste(id)
+    Models::Paste.find(id_b62: params[:id], active: true, spam: false)
+  end
 
   def paste_params
     [:filename, :highlighted, :content]
