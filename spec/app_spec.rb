4dab30b49ea43a37d85639a77e14db9d2899a57f
Ryan Cavicchioni <ryan@cavi.cc>
2014-08-06 08:17:51 -0500

    Added some initial tests.

---
+++ spec/app_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+ENV['RACK_ENV'] = 'test'
+
+require './app'
+require 'rspec'
+require 'rack/test'
+
+describe 'The ctrl-v Application' do
+  include Rack::Test::Methods
+
+  def app
+    App
+  end
+
+  it 'redirects to new' do
+    get '/'
+    expect(last_response).to be_redirect
+    expect(last_response.location).to include('/new')
+  end
+
+  it 'new paste form' do
+    get '/new'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('form')
+  end
+
+  it 'post new paste' do
+    post '/new', { _hp: "", paste: { content: "This is a test." } }
+  end
+end
