7e6adff93d425e1e7aadcda778e6939a8fe6254e
Ryan Cavicchioni <ryan@cavi.cc>
2014-08-24 20:48:44 -0500

    Unicorn should use the shared path instead.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,12 +1,12 @@
-app_path = File.expand_path(File.expand_path(File.dirname(__FILE__)) + '/../../')
+app_path = File.expand_path(File.expand_path(File.dirname(__FILE__)) + '/../../../')
 working_directory app_path
 
 worker_processes (ENV['UNICORN_WORKERS'] || 2).to_i
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared//tmp/sockets/unicorn.sock"
 listen 8080
 
-pid "#{app_path}/tmp/pids/unicorn.pid"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
