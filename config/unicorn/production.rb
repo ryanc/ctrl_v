36db1d4277c21cc35499cad49870e909bd47aca2
Ryan Cavicchioni <ryan@cavi.cc>
2014-08-24 20:47:08 -0500

    Revert "Remove the pid configuration."
    This reverts commit 1290bb39f5b0978f5c9ec3406e17e865b0d286b3.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,6 +6,8 @@ worker_processes (ENV['UNICORN_WORKERS'] || 2).to_i
 listen "#{app_path}/tmp/sockets/unicorn.sock"
 listen 8080
 
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 stdout_path "#{app_path}/log/unicorn.stdout.log"
 
