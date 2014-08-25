0710e76727dd117fc96255b4f3b163bf824a726d
Ryan Cavicchioni <ryan@cavi.cc>
2014-08-24 22:08:17 -0500

    Kill old master processes.

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -21,4 +21,18 @@ GC.respond_to?(:copy_on_write_friendly=) and
 before_fork do |server, worker|
   defined?(DB) and
     DB.disconnect
+
+# This allows a new master process to incrementally
+# phase out the old master process with SIGTTOU to avoid a
+# thundering herd (especially in the "preload_app false" case)
+# when doing a transparent upgrade.  The last worker spawned
+# will then kill off the old master process with a SIGQUIT.
+old_pid = "#{server.config[:pid]}.oldbin"
+if old_pid != server.pid
+  begin
+    sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+    Process.kill(sig, File.read(old_pid).to_i)
+  rescue Errno::ENOENT, Errno::ESRCH
+  end
+end
 end
