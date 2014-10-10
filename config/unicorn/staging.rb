4555a42b6e847f2bfe64bedbcac5d307aec5e4aa
Ryan Cavicchioni <ryan@cavi.cc>
2014-10-10 18:52:32 -0500

    Add unicorn configuration for staging environment.

---
+++ config/unicorn/staging.rb
@@ -0,0 +1,37 @@
+app_path = File.expand_path('../../', File.dirname(__FILE__))
+shared_path = File.expand_path('../../../shared', File.dirname(__FILE__))
+
+working_directory app_path
+
+worker_processes (ENV['UNICORN_WORKERS'] || 3).to_i
+
+listen "#{shared_path}/tmp/sockets/unicorn.sock"
+
+pid "#{shared_path}/tmp/pids/unicorn.pid"
+
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+preload_app true
+
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+  defined?(DB) and
+    DB.disconnect
+
+  # This allows a new master process to incrementally
+  # phase out the old master process with SIGTTOU to avoid a
+  # thundering herd (especially in the "preload_app false" case)
+  # when doing a transparent upgrade.  The last worker spawned
+  # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
