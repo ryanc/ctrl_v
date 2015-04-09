fd882786755c9effdbd69d2dcb064a71ff52167a
Ryan Cavicchioni <ryan@cavi.cc>
2015-04-09 00:08:05 -0500

    Verify that the syntax highlighting checkbox is ticked by default.

--- spec/acceptance/home_spec.rb
+++ spec/acceptance/home_spec.rb
@@ -11,6 +11,11 @@ describe 'The home page' do
     expect(page).to have_css('textarea#paste[autofocus]')
   end
 
+  it 'should default to enabling syntax highlighting' do
+    visit '/new'
+    expect(page).to have_checked_field('Syntax highlighting?')
+  end
+
   it 'should not allow an empty paste' do
     click_button 'Paste It'
     expect(page).to have_content('The paste cannot be blank.')
