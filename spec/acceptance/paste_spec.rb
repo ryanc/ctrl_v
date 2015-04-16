4dd20f6d9eb2333f8a684a84a548b6407d1ae9cc
Ryan Cavicchioni <ryan@cavi.cc>
2015-04-15 23:28:57 -0500

    Display the paste expiration date.
    fixes #10

--- spec/acceptance/paste_spec.rb
+++ spec/acceptance/paste_spec.rb
@@ -9,4 +9,14 @@ describe 'The paste view' do
     visit "/p/#{paste.id_b62}"
     expect(page).to have_content(/Created.+ago/)
   end
+
+  it 'should display the expiration date' do
+    paste = Paste.create(
+      content: "This is a test.",
+      ip_addr: '127.0.0.1',
+      expires_at: Time.now + 3600,
+    )
+    visit "/p/#{paste.id_b62}"
+    expect(page).to have_content(/Expires in/)
+  end
 end
