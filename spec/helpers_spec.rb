24796f8085e5ffeebbb1b4494f8e99f7c0aa5f5b
Ryan Cavicchioni <ryan@cavi.cc>
2015-05-09 14:50:25 -0500

    Git short hashes are hexadecimal.
    refs #19

--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -7,9 +7,9 @@ end
 describe 'sinatra view helpers' do
   it 'should display the application git revision' do
     expect(File).to receive(:readable?).and_return(true)
-    expect(IO).to receive(:read).and_return("abcdefg\n")
+    expect(IO).to receive(:read).and_return("abcdef0\n")
     revision = app_revision
-    expect(revision).to eq("abcdefg")
+    expect(revision).to eq("abcdef0")
   end
 
   it 'should display unknown if application git revision is not present' do
